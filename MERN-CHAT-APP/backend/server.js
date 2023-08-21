f17caf593a4ad31dfb4fbc340b1a737e8c3f5537
subhamdas1999 <subhamdas2101@gmail.com>
2023-08-21 13:44:53 +0530

    allDone

--- MERN-CHAT-APP/backend/server.js
+++ MERN-CHAT-APP/backend/server.js
@@ -5,13 +5,14 @@ const { param } = require('express/lib/request');
 const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
 const chatRoutes = require('./routes/chatRoutes')
+const messageRoutes = require('./routes/messageRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const path = require('path');
+
 
 const app = express();
-
 dotenv.config();
-
 connectDB()
-
 app.use(express.json());  // to accespt JSON data
 
 
@@ -20,16 +21,99 @@ app.get('/', (req, res) => {
 });
 
 
-
 app.use('/api/user',userRoutes)
-
 app.use('/api/chat',chatRoutes)
+app.use('/api/message',messageRoutes)
 
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+const server = app.listen(port, () => { console.log(`Example app listening on port ${port}`);});
+
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
 });
+
+
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+
+socket.on("setup", (userData) => {
+
+        //creating a new room with the ID of the userData
+        // Room will be unique for that user only 
+            socket.join(userData._id);
+            // console.log(userData._id);
+            socket.emit("connected");
+          });
+        
+          // When we click on any of the chat bellow code( L 61 – L 65 )
+          //  should create a room with that particular user and when other
+          //   user joins It will that to this particular room 
+
+          socket.on("join chat", (room) => {
+            socket.join(room);
+            console.log("User Joined Room: " + room);
+          });
+
+
+          socket.on("new message", (newMessageRecieved) => {
+            
+              var chat = newMessageRecieved.chat;
+
+              if (!chat.users) return console.log("chat.users not defined");
+
+              chat.users.forEach((user) => {
+                if (user._id == newMessageRecieved.sender._id) return;
+          
+                socket.in(user._id).emit("message recieved", newMessageRecieved);
+
+              });
+
+          });
+
+
+          socket.off("setup", () => {
+            console.log("USER DISCONNECTED");
+            socket.leave(userData._id);
+
+          });
+ 
+  });
+
+
+ 
+
+
+
+
+
